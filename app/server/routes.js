f413dd3c7e76a965af6438f18290de1050d0897f
Rohan Chougule <rohan183chougule@gmail.com>
2018-04-15 02:08:13 +0530

    final done - 15th april

--- app/server/routes.js
+++ app/server/routes.js
@@ -1,4 +1,4 @@
-process.env.NODE_ENV = "production";
+process.env.NODE_ENV = "default";
 
 console.log("========================= NODE ENVIRONMENT : " + process.env.NODE_ENV + "============================\n")
 
